7f57a70768ec1241f045ffe3eca1888257638cc4
Seth Bowman <brandonsethbowman@gmail.com>
2023-05-22 09:24:22 -0500

    Example made

---
+++ ClassDemo/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    public class Person
+    {
+        //Field
+        public int money;
+
+        //Default Constructor
+        public Person()
+        {
+
+        }
+
+        //Custom Constructor
+        public Person(string name, int age, bool isMarried, string hairColor, double weight)
+        {
+            Name = name;
+            Age = age;
+            IsMarried = isMarried;
+            HairColor = hairColor;
+            Weight = weight;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsMarried { get; set; }
+        public string HairColor { get; set; }
+        public double Weight { get; set; }
+
+        //Method in class (non static)
+        public void Greet()
+        {
+            Console.WriteLine($"Hello! My name is {Name} and I am {Age} years old.");
+        }
+    }
+}
---
+++ ClassDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassDemo;
+
+//Dot Notation
+var seth = new Person();
+seth.Name = "Seth";
+seth.Age = 29;
+seth.IsMarried = true;
+seth.Weight = 155.55;
+
+seth.Greet();
+
+//Object Initializer Syntax
+var john = new Person() 
+{ 
+    Name = "John",
+    Age = 32 
+};
+
+john.Greet();
+
+//Using a custom constructor
+var monica = new Person("Monica", 27, true, "brown", 100);
+monica.Greet();
